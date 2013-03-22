c34081c4e8c19a5085b4e859e73ffb1949570cf1
Chris Kempson <git@chriskempson.com>
2013-03-22 12:23:22 +0000

    Update emoji.rb
    Emoji dir is now automatically created.
    Emoji files are copied to source as a generator.
    Image tag no longer has hardcoded height and width.

--- emoji.rb
+++ emoji.rb
@@ -8,36 +8,29 @@
 # a full listing of emoji codes.
 #
 # Installation:
+#   - Run `gem install gemoji` or add `gem 'gemoji'` to your gemfile and run `bundle install`
 #   - Copy this file to your `_plugins` directory
 #   - Add a line like `emoji: ./images/emoji` to your `_config.yml`
-#   - Make the `./images/emoji/` directory
 # 
 # Usage: 
 #   - Apply the filter wherever needed e.g. {{ content | emojify }}
 #   - Add some emoji to your article! e.g. "Hello :wink:"
 
-require "gemoji"
+require 'gemoji'
 
 module Jekyll
+
   module EmojiFilter
 
-    def copy_emoji
-      Dir["#{Emoji.images_path}/emoji/*.png"].each do |src|
-        FileUtils.cp src, @context.registers[:site].config['emoji']
-      end
-    end
-
     def emojify(content)
-      path = @context.registers[:site].config['emoji']
-      abs_path = path.gsub(".", "")
+      return false if !content
 
-      if !path.empty? and !File.exists?(path + '/smiley.png') 
-        copy_emoji
-      end
+      config = @context.registers[:site].config
+      emoji_dir = File.join(config['source'], config['emoji_dir'])
 
       content.to_str.gsub(/:([a-z0-9\+\-_]+):/) do |match|
-        if Emoji.names.include?($1) and path
-          '<img alt="' + $1 + '" height="20" src="' + abs_path + "/#{$1}.png" + '" class="emoji" width="20" />'
+        if Emoji.names.include?($1) and emoji_dir
+          '<img alt="' + $1 + '" src="/' + config['emoji_dir'] + "/#{$1}.png" + '" class="emoji" />'
         else
           match
         end
@@ -45,6 +38,24 @@ module Jekyll
     end
 
   end
+
+  class EmojiGenerator < Generator
+    def generate(site)
+      config = site.config
+      emoji_dir = File.join(config['source'], config['emoji_dir'])
+
+      return false if File.exist?(emoji_dir + '/smiley.png')
+
+      # Make Emoji directory
+      FileUtils.mkdir_p(emoji_dir)
+
+      # Copy Gemoji files
+      Dir["#{Emoji.images_path}/emoji/*.png"].each do |src|
+        FileUtils.cp src, emoji_dir
+      end
+    end
+  end
+
 end
 
 Liquid::Template.register_filter(Jekyll::EmojiFilter)
